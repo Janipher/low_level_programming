54b67926e706b04bc31e76aa55621929ecbc434d
Janipher <janipherowiny@gmail.com>
2022-09-10 03:16:28 -0700

    My first C program

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+
+/**
+* main - print the string in the puts function
+*
+* Description: using the main function 
+* this string should print "Programming is like building a multilingual puzzle
+* Return: 0
+*/
+int main(void)
+{
+         puts("/"Programming is like building a multilingual puzzle");
+         return (0);
+}
