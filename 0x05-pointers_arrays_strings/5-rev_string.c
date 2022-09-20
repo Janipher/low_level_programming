17b934684a37464af53beb7017f9bfdde64eda4e
root <janipherowinyy@gmail.com>
2022-09-20 14:18:29 -0700

    Reverses a string

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_rev - print reverse characters.
+ * @s: validate the character
+ * Return: Always 0
+ */
+
+void print_rev(char *s)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+	i--;
+	for (; i >= 0; i--)
+	{
+		putchar(s[i]);
+	}
+	putchar('\n');
+}
